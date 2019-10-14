2d89e6645a4707c2c8ede5572b2fec3a063bd42d
Jashele T <jashele@Jasheles-Air.domain>
2019-10-14 06:45:07 -0700

    changed the login credentials in server js to something easier

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,32 +1,66 @@
 import React from "react";
-
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 class Login extends React.Component {
-
   state = {
     credentials: {
       username: "",
       password: ""
     }
-  }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", this.state.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/colors");
+      })
+      .catch(err => console.log(err));
+  };
+
 
 
   render() {
     return (
-<>
-      <h1>Welcome to the Bubble App!</h1>
-      
-      
-      <form>
-        <input type="text" name="username" placeholder="username" />
-        <input type="password" name="password"placeholder="password" />
-        <button type="submit">Login</button>
-      </form>
-    </>
-    )
+      <>
+        <h1>Welcome to the Bubble App!</h1>
+
+        <form>
+          <input 
+          type="text" 
+          name="username" 
+          placeholder="type user" 
+          />
+
+
+          <input 
+          type="password" 
+          name="password" 
+          placeholder="type password" 
+          />
+
+
+          <button 
+          type="submit">
+            Login
+            </button>
+
+
+        </form>
+      </>
+    );
   }
 }
-
-
 
 export default Login;
