883864f231a180e4747919a4283c7497c72bc1f8
Jashele T <jashele@Jasheles-Air.domain>
2019-10-14 06:39:22 -0700

    got the login form up

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,32 @@
 import React from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
+
+class Login extends React.Component {
+
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  }
+
+
+  render() {
+    return (
+<>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      
+      
+      <form>
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password"placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
     </>
-  );
-};
+    )
+  }
+}
+
+
 
 export default Login;
